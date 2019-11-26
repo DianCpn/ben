faea198fc9ac5ad03e1465d379e3432d5fb8c0bf
Diana Capunay <diana.capunay-jara@hotmail.com>
2019-11-26 18:38:56 +0100

    file openfoodfacts_service.rb created

---
+++ app/services/openfoodfacts_service.rb
@@ -0,0 +1,22 @@
+require 'httparty'
+require 'json'
+
+# class OpenfoodfactsService
+class OpenfoodfactsService
+  def initialize(upc)
+    @upc = upc
+  end
+
+  def call
+    response = HTTParty.get("https://world.openfoodfacts.org/api/v0/product/#{@upc}.json")
+  end
+endgit
+
+# faire a la main ou seeder le type de packaging
+test_upc = 5410041010800
+puts OpenfoodfactsService.new(test_upc).call
+
+# product.title = response["product"]["product_name_fr"]
+# product.brand = response["product"]["brands"]
+# product.image_url = response["product"]["image_url"]
+# puts "#{product.title}, #{product.brand}, #{product.image_url}"
