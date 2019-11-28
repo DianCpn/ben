a512972e9762441d727d4ca0667703917aef5b61
Jean-Francois Medard <jeanfmedard@gmail.Com>
2019-11-28 10:17:39 +0100

    product controller WIP

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,23 +7,118 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+# dodo la sardine 3263670162219
+# stir fry doudou 737628064502
+# chips : 5060042640515
+# pitch choco : 3256540000636
 
   def create
-    response = HTTParty.get("https://world.openfoodfacts.org/api/v0/product/#{product_params[:upc]}.json")
     @product = Product.new(product_params)
-    @product.brand = response["product"]["brands_tags"][0]
-    @product.title = response["product"]["product_name_fr"]
-    @product.image_url = response["product"]["image_url"]
+    response = OpenfoodfactsService.new(3256540000636).call
+
+
+    @product.title = response["product"]["product_name"]
+    @product.brand = response["product"]["brands"]
     @product.save
-    @product_package = Product_package.new
+    @package = response["product"]["packaging"]
+    package_array = @package.downcase.gsub(" ", "").split(",")
+
+    hash_sort = {
+      carton: ["carton"],
+      glass: ["verre", "glass", "bocal"],
+      plastic: ["plastique", "plastic"],
+      metal: ["aluminum"]
+    }
+
+    result_carton = package_array.map do |item|
+      hash_sort[:carton].include?(item)
+    end
+
+    result_glass = package_array.map do |item|
+      hash_sort[:glass].include?(item)
+    end
+
+    result_plastic = package_array.map do |item|
+      hash_sort[:plastic].include?(item)
+    end
+
+    result_metal = package_array.map do |item|
+      hash_sort[:metal].include?(item)
+    end
+
+
+    @product_package = ProductPackage.new
     @product_package.product = @product
-    # rattachement du packaging au produit? fait à la console en manuel ou en seed
-    @product_package.save
-    @search = Search.new
-    @search.user = current_user
-    @search.product = @product
-    @search.save
-    redirect_to product_path(@product)
+    result_glass.include?(true)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # response = HTTParty.get("https://world.openfoodfacts.org/api/v0/product/#{product_params[:upc]}.json")
+    # @product = Product.new(product_params)
+    # @products = Product.all
+
+    #  @products.each do |product|
+    #  product.upc == @product.upc
+    # end
+    #   redirect_to product_path(@product)
+    # else
+    #   if response["product"].nil?
+    #     @product.brand = "TBD"
+    #   else
+    #     @product.brand = response["product"]["brands_tags"][0]
+    #   end
+    #   if response["product"].nil?
+    #     @product.title = "TBD"
+    #   else
+    #     @product.title = response["product"]["product_name_fr"]
+    #   end
+    #   if response["product"].nil?
+    #     @product.image_url = "TBD"
+    #   else
+    #     @product.image_url = response["product"]["image_url"]
+    #   end
+    #   @product.save
+    # end
+    # @product_package = ProductPackage.new
+    # @product_package.product = @product
+    # # rattachement du packaging au produit? fait à la console en manuel ou en seed
+    # @product_package.save
+    # @search = Search.new
+    # @search.user = current_user
+    # @search.product = @product
+    # @search.save
+    # redirect_to product_path(@product)
   end
 
   private
