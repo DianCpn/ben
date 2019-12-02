9755bce59c9edef02500abcd07ba2e35f6c8000a
Jean-Francois Medard <jeanfmedard@gmail.Com>
2019-12-02 16:02:18 +0100

    refined scan-no modal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_28_143407) do
+ActiveRecord::Schema.define(version: 2019_12_02_084656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_11_28_143407) do
     t.datetime "updated_at", null: false
     t.string "instruction"
     t.string "icon"
+    t.string "tip"
   end
 
   create_table "product_packages", force: :cascade do |t|
