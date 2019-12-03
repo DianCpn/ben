79cd9d49032a8c3a74de8f76bd211f1d0ecf036d
Jean-Francois Medard <jeanfmedard@gmail.Com>
2019-12-03 10:19:36 +0100

    search page back

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
 
   get "/favorites", to: "searches#favorites"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/find", to: "containers#find"
 end
