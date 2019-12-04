44873b95aaa44c22c92381507fb622316cfab2f8
Jean-Francois Medard <jeanfmedard@gmail.Com>
2019-12-04 12:18:29 +0100

    favorites are ok

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,17 @@ class Product < ApplicationRecord
   has_many :searches, dependent: :destroy
   has_one_attached :photo
   # validates :upc, uniqueness: true
+
+  def color_name
+    all_packagings_colors = []
+    product_packages.each do |product_package|
+      all_packagings_colors << product_package.packaging.color_name
+    end
+    unique_colors = all_packagings_colors.uniq
+    if unique_colors.count == 1
+      return unique_colors[0]
+    else
+      "neutral"
+    end
+  end
 end
