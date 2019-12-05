76201978dc93285b25a494de9a452e0cd7fd3e2e
Jean-Francois Medard <jeanfmedard@gmail.Com>
2019-12-05 09:59:49 +0100

    added open graph meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::Base
   PLASTIC = ["plastique", "plastic", "sachet"]
 
   before_action :authenticate_user!
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
